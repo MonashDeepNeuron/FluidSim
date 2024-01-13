5f58ef7f7ac774ce6a04b1a7fa727ee66eb0cc98
Kevin Duignan <kevin.nguyen200476@gmail.com>
2024-01-13 16:24:55 +1100

    Add fluid class prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <SFML/Graphics.hpp>
 
+// Define macro for converting 2D array coordinates into 1D
+#define IX(i, j) ((i) + (N + 2) * (j))
+
 // Define grid size
 constexpr int N = 100;
 constexpr int SIZE = (N + 2) * (N + 2);
+
+// TODO: Define fluid class
+/* This class will contain all the variables and methods necessary to solve
+    the Navier-Stokes equation for both density and velocity. */
+class Fluid {
+  std::vector<float> u, v, u_prev, v_prev, dens, dens_prev;
+  float dt, diff, visc;
+};
 
 int main() {
   auto window = sf::RenderWindow{{1920u, 1080u}, "CMake SFML Project"};
