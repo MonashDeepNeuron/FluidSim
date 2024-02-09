4520d2331b9fbd9a776cb80ac502539338694cfb
Kevin Duignan <kevin.nguyen200476@gmail.com>
2024-02-09 22:28:54 +1100

    Change project name for windows config

--- headers/const.hpp
+++ headers/const.hpp
@@ -2,7 +2,7 @@
 
 constexpr int N = 100;
 constexpr int SIZE = (N + 2) * (N + 2);
-constexpr int CELL_SIZE = 15;
+constexpr int CELL_SIZE = 5;
 
 #define IX(i, j) (i + (N + 2) * j)
 #define SWAP(arr1, arr2) std::swap(arr1, arr2)
