81f641a86226106f89ad02b86826edcfeaef44d7
Aydin <thefickers@outlook.com>
2024-03-09 22:03:54 +1100

    Implemented function HSV_to_SFML so we can print to colour
    TODO: Do this same thing with the velocity field once its been created / implemented correctly

--- headers/gui.hpp
+++ headers/gui.hpp
@@ -140,12 +140,49 @@ public:
                 sf::RectangleShape pixel(sf::Vector2f(CELL_SIZE, CELL_SIZE));
                 pixel.setPosition(xpos, ypos);
 
-                auto value = grey_scale(data.at(IX(j, i)));
+                auto value = data.at(IX(i, j));
+                auto color = HSV_to_RGB(value * 360.0f, 1.0f, 1.0f); // Assuming value is in the range [0, 1]
 
-                pixel.setFillColor(sf::Color(value, value, value));
+                pixel.setFillColor(color);
 
                 window.draw(pixel);
             }
         }
+    }
+
+    auto HSV_to_RGB(float H, float S, float V) -> sf::Color
+    {
+        float C = S * V;
+        float X = C * (1.0f - std::abs(fmodf(H / 60.0f, 2.0f) - 1.0f));
+        float m = V - C;
+        float Rs, Gs, Bs;
+
+        if (H >= 0.0f && H < 60.0f) {
+            Rs = C;
+            Gs = X;
+            Bs = 0.0f;
+        } else if (H >= 60.0f && H < 120.0f) {
+            Rs = X;
+            Gs = C;
+            Bs = 0.0f;
+        } else if (H >= 120.0f && H < 180.0f) {
+            Rs = 0.0f;
+            Gs = C;
+            Bs = X;
+        } else if (H >= 180.0f && H < 240.0f) {
+            Rs = 0.0f;
+            Gs = X;
+            Bs = C;
+        } else if (H >= 240.0f && H < 300.0f) {
+            Rs = X;
+            Gs = 0.0f;
+            Bs = C;
+        } else {
+            Rs = C;
+            Gs = 0.0f;
+            Bs = X;
+        }
+
+        return sf::Color(static_cast<sf::Uint8>((Rs + m) * 255.0f), static_cast<sf::Uint8>((Gs + m) * 255.0f), static_cast<sf::Uint8>((Bs + m) * 255.0f));
     }
 };
