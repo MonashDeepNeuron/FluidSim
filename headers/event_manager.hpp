74a7cd48354a707129083f73c3ea292086b8fb6f
Kevin Duignan <kevin.nguyen200476@gmail.com>
2024-04-01 14:03:35 +1100

    add boundary checks for event manager - no input

--- headers/event_manager.hpp
+++ headers/event_manager.hpp
@@ -36,7 +36,9 @@ public:
             auto x = event.mouseButton.x / S_CELL_SIZE;
             auto y = event.mouseButton.y / S_CELL_SIZE;
 
-            if (x < 0 || S_AXIS_SIZE < x || y < 0 || S_AXIS_SIZE < y) [[unlikely]] {
+            if (x <= 0 || y <= 0 || y >= S_AXIS_SIZE || x >= S_AXIS_SIZE) [[unlikely]] { // Don't do anything for boundary conditions
+                // fmt::println("Left mouse button was pressed at ({}, {})", x, y);
+                // fmt::println("yes");
                 return 0;
             }
 
@@ -68,12 +70,14 @@ public:
 
             auto x = mousePos.x / S_CELL_SIZE;
             auto y = mousePos.y / S_CELL_SIZE;
-
-            if (0 <= x && x <= S_AXIS_SIZE && 0 <= y && y <= S_AXIS_SIZE) [[likely]] {
-                //fmt::println("Left mouse button is being held down at ({}, {})", x, y);
-
-                return IX(sign_cast(y), sign_cast(x));
+            
+            if (x <= 0 || y <= 0 || y >= S_AXIS_SIZE || x >= S_AXIS_SIZE) [[unlikely]] { // Don't do anything for boundary conditions
+                // fmt::println("Left mouse button was pressed at ({}, {})", x, y);
+                // fmt::println("yes");
+                return 0;
             }
+
+            return IX(sign_cast(y), sign_cast(x));
         }
         return 0;
     }
